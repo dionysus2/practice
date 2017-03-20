e221564b1522952754276de322cff4cc44550cb0
bstsay <bstsay@naver.com>
2017-03-20 16:18:14 +0900

    되나?

--- dionysus/src/dionysus/wine/dao/WineDeliveryInfoDAO.java
+++ dionysus/src/dionysus/wine/dao/WineDeliveryInfoDAO.java
@@ -2,4 +2,5 @@ package dionysus.wine.dao;
 
 public interface WineDeliveryInfoDAO {
 
+
 }
--- dionysus/src/dionysus/wine/dao/WineInfoDAO.java
+++ dionysus/src/dionysus/wine/dao/WineInfoDAO.java
@@ -2,4 +2,25 @@ package dionysus.wine.dao;
 
 public interface WineInfoDAO {
 
+	/*
+	 * 												리턴				매개변수	
+	 *와인상품 전체리스트 조회			ArrayList<wineInfo>				Connection, wineinfo
+	 *와인상품 원산지별 조회			ArrauList<wineInfo>				Connection, 원산지
+	 *와인상품 콜키지정보				보류
+	 *와인상품 추가						int								Connection, 와인이름
+	 *와인상품 정보수정					wineInfo 						Connection, 와인이름
+	 *와인상품 삭제						int								Connection, 와인이름
+	 *상품별 상품등록 회사리스트조회 	ArrayList<wineInfo>				Connection, 등록회사,와인번호
+	 *단가별 조회						ArrayList<wineInfo>				Connection, 와인가격
+	 *상품별 리뷰관리					winereview						Connection, 상품리뷰
+	 *회사별 상품조회					ArrayList<wineInfo>				Connection, 회사이름
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
 }
