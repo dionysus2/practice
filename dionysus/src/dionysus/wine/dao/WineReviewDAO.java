9faa93fa0637fa19934305d17fb145ead64a9856
Administrator <Administrator@Admin-PC>
2017-03-20 16:23:10 +0900

    WineOrderDAO WineOrderInfoDAO WineReviewDAO WineReviewDAO WineSellerAuthorizationDAO

--- dionysus/src/dionysus/wine/dao/WineReviewDAO.java
+++ dionysus/src/dionysus/wine/dao/WineReviewDAO.java
@@ -1,5 +1,14 @@
 package dionysus.wine.dao;
 
 public interface WineReviewDAO {
-
+	/*
+	 * 	- 상품별 리뷰정보 등록						리턴					매개변수
+	 * 		1. 상품별 리뷰정보 등록					int						Connection, 상품번호, 고객번호, WineReview
+	 * 	- 상품별 리뷰정보 수정				
+	 * 		2. 상품별 리뷰정보 수정					int						Connection, 상품번호, 고객번호, WineReview
+	 * 	- 상품별 리뷰정보 삭제
+	 * 		3. 상품별 리뷰정보 삭제					int						Connection, 상품번호, 고객번호
+	 * 	- 상품별 리뷰 전체리스트 조회
+	 * 		4. 상품별 리뷰정보 전체리스트 조회		ArrayList<WineReview>	Connection, 상품번호
+	 * */
 }
