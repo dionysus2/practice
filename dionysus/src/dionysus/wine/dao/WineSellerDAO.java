6a78b7dc6aedb7bfa2485067d32e39aef7534876
Administrator <Administrator@Admin-PC>
2017-03-20 16:38:54 +0900

    dao 두개 추가

--- dionysus/src/dionysus/wine/dao/WineSellerDAO.java
+++ dionysus/src/dionysus/wine/dao/WineSellerDAO.java
@@ -1,5 +1,16 @@
 package dionysus.wine.dao;
 
 public interface WineSellerDAO {
+	/*
+	 *  와인 상품관리                              리턴                         매개변수
+	 *  1. 와인상품 전체리스트 출력           ArrayList                    Connection
+	 *  2. 와인상품 원산지별 조회              ArrayList                 Connection,  원산지
+	 *  3. 와인상품 추가                           int                          Connection
+	 *  4. 와인상품 상세접근                      int                          Connection  
+	 *  5. 와인상품 정보수정                      int                          Connection 
+	 *  6. 와인상품 리스트내 삭제                int                      Connection,  와인번호
+	 *  7. 와인상품 콜키지정보               ResCorkageInfo              Connection                                      
+	 */
+
 
 }
