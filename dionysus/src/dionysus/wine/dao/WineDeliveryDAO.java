0cda69494a2e67adcdc037afdb7b445cf024e7de
bstsay <bstsay@naver.com>
2017-03-21 08:39:41 +0900

    수정과

--- dionysus/src/dionysus/wine/dao/WineDeliveryDAO.java
+++ dionysus/src/dionysus/wine/dao/WineDeliveryDAO.java
@@ -3,7 +3,7 @@ package dionysus.wine.dao;
 public interface WineDeliveryDAO {
 	/*
 	 *					 				리턴			 		매개변수
-	 *와인 배송번호로 조회				int						Connection, 배송번호
+	 *배송번호로 조회					winedelivery			Connection, 배송번호
 	 *회원번호로 배송리스트 조회	ArrayList<WineDelivery>		Connection, 회원번호
 	 *배송진행 사항 수정				String					Connection, 배송진행사항
 	 * 1
--- dionysus/src/dionysus/wine/dao/WineServiceGuideDAO.java
+++ dionysus/src/dionysus/wine/dao/WineServiceGuideDAO.java
@@ -3,5 +3,6 @@ package dionysus.wine.dao;
 public interface WineServiceGuideDAO {
 	/*
 	 * 	보류
+	 * 	보류
 	 * */
 }
