2494919d392a315b526bffc8b83e021feaadee22
bstsay <bstsay@naver.com>
2017-03-21 09:41:14 +0900

    1

--- dionysus/src/dionysus/wine/dao/WineDeliveryDAO.java
+++ dionysus/src/dionysus/wine/dao/WineDeliveryDAO.java
@@ -4,11 +4,12 @@ public interface WineDeliveryDAO {
 	/*
 	 *					 				리턴			 		매개변수
 	 *배송 추가							int						Connection, WineDelivery
-	 *배송 리스트 조회				ArrayList<WineDelivery>		Connection
+	 *배송 리스트 조회					ArrayList<WineDelivery>	Connection
 	 *배송번호로 조회					WineDelivery			Connection, 배송번호
-	 *회원번호로 배송리스트 조회	ArrayList<WineDelivery>		Connection, 회원번호
+	 *회원이름으로 배송리스트 조회		ArrayList<WineDelivery>	Connection, 회원이름
 	 *배송진행 사항 수정				String					Connection, 배송진행사항
-	 *
+	 *와인회사명으로 배송리스트 조회	ArrayList<WineDelivery>	Connection, 와인회사명
+	 *레스토랑명으로 배송리스트 조회	ArrayList<WineDelivery>	Connection, 레스토랑명
 	 */
 	
 	
