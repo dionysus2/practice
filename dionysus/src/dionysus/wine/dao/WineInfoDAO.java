6512810f7c8f8dfb2db8bf927f56e79e70bf936d
Administrator <Administrator@Admin-PC>
2017-03-21 11:36:51 +0900

    와인상품 인터페이스 작성

--- dionysus/src/dionysus/wine/dao/WineInfoDAO.java
+++ dionysus/src/dionysus/wine/dao/WineInfoDAO.java
@@ -1,25 +1,39 @@
 package dionysus.wine.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dionysus.wine.vo.WineInfo;
+
 public interface WineInfoDAO {
 
 	/*
 	 * 												리턴				매개변수	
-	 *와인상품 전체리스트 조회			ArrayList<wineInfo>				Connection, wineinfo
-	 *	 *단가높은순 조회					ArrayList<wineInfo>			Connection, 와인가격
-	 *단가낮은순 조회					ArrayList<WineInfo>				Connection, 와인가격
-	 *	와인상품 Count						int							Connection
+	 *	와인상품 전체리스트 조회		ArrayList<wineInfo>			Connection
+	 *	단가높은순 조회					ArrayList<wineInfo>			Connection, 와인가격
+	 *	단가낮은순 조회					ArrayList<WineInfo>			Connection, 와인가격
+	 *	와인상품 Count						int						Connection
 	 *
-	 *와인상품 원산지별 조회			ArrauList<wineInfo>				Connection, 원산지
-	 *와인상품 원산지별 Count			int							Connection
+	 *	와인상품 원산지별 조회			ArrauList<wineInfo>				Connection, 원산지
+	 *	와인상품 원산지별 Count			int							Connection
 	 *
-	 *와인상품 추가						int								Connection, 와인이름
-	 *와인상품 정보수정					wineInfo 						Connection, 와인이름
-	 *와인상품 삭제						int								Connection, 와인이름
-	 *상품별 상품등록 회사리스트조회 	ArrayList<wineInfo>				Connection, 등록회사,와인번호
-	 *와인회사별 Count						int							Connection
-	 *
-	 *회사별 상품조회					ArrayList<wineInfo>				Connection, 회사이름
+	 *	와인상품 추가						int								Connection, WineInfo
+	 *	와인상품 정보수정					wineInfo 						Connection,	WineInfo
+	 *	와인상품 삭제						int								Connection, 와인이름
+	 *	회사리스트별 상품조회 	ArrayList<wineInfo>				Connection, 회사이름
+	 *	와인회사별 Count						int							Connection
 	 * 
 	 */
-
+	public ArrayList<WineInfo> selectAllWineInfo(Connection conn)throws SQLException;
+	public ArrayList<WineInfo> selectWinePriceMax(Connection conn, int wineInfoPrice)throws SQLException;
+	public ArrayList<WineInfo> selectWinePriceMin(Connection conn, int wineInfoPrice)throws SQLException;
+	public int wineInfoCount(Connection conn)throws SQLException;
+	public ArrayList<WineInfo> selectWineOrigin(Connection conn, String wineInfoOrigin)throws SQLException;
+	public int wineOriginCount(Connection conn)throws SQLException;
+	public int wineInfoInsert(Connection conn, WineInfo wine)throws SQLException;
+	public int wineInfoUpdate(Connection conn, WineInfo wine)throws SQLException;
+	public int wineInfoDelete(Connection conn, String wineInfoName)throws SQLException;
+	public ArrayList<WineInfo> selectByWineSellerWineInfo(Connection conn, String wineInfoName)throws SQLException;
+	public int wineSellerWineInfoCount(Connection conn)throws SQLException;
 }
