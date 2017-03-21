7bed4d12e1c3bc6ca8ea09e85a96f387330995d9
Administrator <Administrator@Admin-PC>
2017-03-21 09:54:01 +0900

    WineSeller 주석변경

--- dionysus/src/dionysus/wine/dao/WineSellerDAO.java
+++ dionysus/src/dionysus/wine/dao/WineSellerDAO.java
@@ -2,15 +2,26 @@ package dionysus.wine.dao;
 
 public interface WineSellerDAO {
 	/*
-	 *  와인 상품관리                              리턴                         매개변수
-	 *  1. 와인상품 전체리스트 출력           ArrayList                    Connection
-	 *  2. 와인상품 원산지별 조회              ArrayList                 Connection,  원산지
-	 *  3. 와인상품 추가                           int                          Connection
-	 *  4. 와인상품 상세접근                      int                          Connection  
-	 *  5. 와인상품 정보수정                      int                          Connection 
-	 *  6. 와인상품 리스트내 삭제                int                      Connection,  와인번호
-	 *  7. 와인상품 콜키지정보               ResCorkageInfo              Connection                                      
-	 */
-
-
+	 *  - 와인회사 업주 회원 비활성화 설정.							리턴						매개변수
+	 * 		1. 와인회사 업주 회원 비활성화 설정						int							Connection
+	 * 		(비활성화:0 활성화:1 업주회원은 회원가입 신청 시 비활성화 속성은 기본으로 0으로 강제지정)
+	 * 	- 와인회사 업주 회원 페이지별 리스트 조회.
+	 * 		2. 와인회사 업주 페이지별 리스트 조회					ArrayList<WineSeller>		Connection
+	 * 		3. 와인회사 업주 회원 count								int							Connection
+	 * 	- 와인회사 업주 회원 지역별/페이지별 리스트 조회
+	 * 		4. 와인회사 업주 회원 지역별 조회						ArrayList<WineSeller>		Connection, 지역
+	 * 	- 와인회사 업주 회원신청 승인.
+	 * 		5. 와인회사 업주 회원신청자 비활성화 속성 1로 변경.		int							Connection
+	 * 	- 와인회사 업주 회원 추가
+	 * 		6. 와인회사 업주 회원추가								int							Connection, WineSeller
+	 * 		(회원추가 시 WineAuthorization테이블로 이동)
+	 * 	- 로그인
+	 * 		7. 와인회사 업주 회원로그인								int							Connection, 아이디, 비밀번호
+	 * 	- 로그아웃(컨트롤단에서 처리)
+	 * 	- 와인회사 업주 회원 아이디 찾기
+	 * 		8. 와인회사 업주 회원 아이디 찾기						String						Connection, 이름, 사업자번호			
+	 * 	- 와인회사 업주 회원 비밀번호 찾기
+	 * 		9. 와인회사 업주 회원 비밀번호 찾기						String						Connection, 아이디, 이름, 사업자번호 
+	 * */
+	
 }
