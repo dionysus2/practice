1d354aadb3aa6f766835db61fcedc448ff90bee7
Administrator <Administrator@Admin-PC>
2017-03-22 16:50:24 +0900

    WineInfo서비스단 인터페이스 작성

--- dionysus/src/dionysus/wine/service/WineInfoService.java
+++ dionysus/src/dionysus/wine/service/WineInfoService.java
@@ -1,5 +1,24 @@
 package dionysus.wine.service;
 
-public interface WineInfoService {
+import javax.servlet.http.HttpServletRequest;
 
+public interface WineInfoService {
+	/*											
+	 *	와인상품 전체리스트 조회		
+	 *	단가높은순 조회					
+	 *	단가낮은순 조회					
+	 *	와인상품 원산지별 조회			
+	 *	와인상품 추가						
+	 *	와인상품 정보수정					
+	 *	와인상품 삭제						
+	 *	회사리스트별 상품조회 	
+	 */
+	public String readAllWineInfo(HttpServletRequest request);
+	public String readPriceMax(HttpServletRequest request);
+	public String readPriceMin(HttpServletRequest request);
+	public String readOriginWineInfo(HttpServletRequest request);
+	public String wineInfoCreate(HttpServletRequest request);
+	public String wineInfoUpdate(HttpServletRequest request);
+	public String wineInfoDelete(HttpServletRequest request);
+	public String readWineSellerWineInfo(HttpServletRequest request);
 }
