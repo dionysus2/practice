462cdd8523298f9a123dfc972bd57c3a4683d5f3
Administrator <Administrator@Admin-PC>
2017-03-22 15:25:41 +0900

    수정

--- dionysus/src/dionysus/wine/dao/WineInfoDAO.java
+++ dionysus/src/dionysus/wine/dao/WineInfoDAO.java
@@ -13,14 +13,14 @@ public interface WineInfoDAO {
 	 *	단가낮은순 조회					ArrayList<WineInfo>			Connection, 와인가격
 	 *	와인상품 Count						int						Connection
 	 *
-	 *	와인상품 원산지별 조회			ArrauList<wineInfo>				Connection, 원산지
-	 *	와인상품 원산지별 Count			int							Connection
+	 *	와인상품 원산지별 조회			ArrauList<wineInfo>					Connection, 원산지
+	 *	와인상품 원산지별 Count			int									Connection
 	 *
 	 *	와인상품 추가						int								Connection, WineInfo
 	 *	와인상품 정보수정					wineInfo 						Connection,	WineInfo
 	 *	와인상품 삭제						int								Connection, 와인이름
-	 *	회사리스트별 상품조회 	ArrayList<wineInfo>				Connection, 회사이름
-	 *	와인회사별 Count						int							Connection
+	 *	회사리스트별 상품조회 	ArrayList<wineInfo>							Connection, 회사이름
+	 *	와인회사별 Count						int							Connection, 회사번호
 	 */
 	public ArrayList<WineInfo> selectAllWineInfo(Connection conn, int startRow, int lastRow)throws SQLException;
 	public ArrayList<WineInfo> selectWinePriceMax(Connection conn, int wineInfoPrice, int startRow, int lastRow)throws SQLException;
