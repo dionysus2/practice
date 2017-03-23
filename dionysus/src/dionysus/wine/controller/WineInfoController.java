df9ca3d01174da70d241ca7657f1fd8db0a6adec
Administrator <Administrator@Admin-PC>
2017-03-23 09:29:45 +0900

    WineInfoController 간략 테스트 작성

--- dionysus/src/dionysus/wine/controller/WineInfoController.java
+++ dionysus/src/dionysus/wine/controller/WineInfoController.java
@@ -1,5 +1,30 @@
 package dionysus.wine.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dionysus.wine.di.ModelAndView;
+import dionysus.wine.di.RequestMapping;
+import dionysus.wine.serviceimpl.WineInfoServiceImpl;
+
 public class WineInfoController {
-	
+	private Logger logger= LoggerFactory.getLogger(WineInfoController.class);
+	@RequestMapping(value="/wineinfo/insert", method="GET")
+	public static ModelAndView insertStart(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		return mav;
+	}
+	@RequestMapping(value="/wineinfo/insert", method="POST")
+	public static ModelAndView inserEnd(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.addObject("result", service.wineInfoCreateEnd(request));		
+		//	result success일때, fail일때 다른view 이동 추가코딩 필요함
+		mav.setView("list");				
+		//	상품추가 후 이동페이지 설정 다시정할 필요있음.
+		mav.setRedirect();
+		return mav;
+	}
 }
