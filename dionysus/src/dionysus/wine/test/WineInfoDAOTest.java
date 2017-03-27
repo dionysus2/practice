286a4a890c2a98bb488a5340238a435d1973f6a6
Administrator <Administrator@Admin-PC>
2017-03-27 10:40:52 +0900

    와인정보 DAO 테스트완료

--- dionysus/src/dionysus/wine/test/WineInfoDAOTest.java
+++ dionysus/src/dionysus/wine/test/WineInfoDAOTest.java
@@ -1,5 +1,73 @@
 package dionysus.wine.test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import dionysus.wine.daoimpl.WineInfoDAOImpl;
+import dionysus.wine.util.JDBCUtil;
+import dionysus.wine.vo.WineInfo;
+
 public class WineInfoDAOTest {
 
+	//	@Test
+	//	성공
+	public void selecAllWineInfoTest(){
+		Connection conn= JDBCUtil.getConnection();
+		int startRow= 1;
+		int lastRow= 10;
+		try {
+			ArrayList<WineInfo>result= new WineInfoDAOImpl().selectAllWineInfo(conn, startRow, lastRow);
+			assertThat(result.size(), is(1));
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			JDBCUtil.close(conn);
+		}
+	}
+	//	@Test
+	//	성공
+	public void wineInfoCountTest(){
+		Connection conn= JDBCUtil.getConnection();
+		try {
+			int result= new WineInfoDAOImpl().wineInfoCount(conn);
+			assertThat(result, is(1));
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			JDBCUtil.close(conn);
+		}
+	}
+	@Test
+	public void insertTest(){
+		Connection conn= JDBCUtil.getConnection();
+		String wineInfoName= "테스트";
+		String wineInfoProfilePicture="테스트";
+		int wineInfoPrice= 9999;
+		String wineInfoOrigin= "국내산";
+		String wineInfoPicture1="테스트";
+		String wineInfoPicture2="테스트";
+		String wineInfoPicture3="테스트";
+		int wineSellerId=1;
+		try {
+			int result= new WineInfoDAOImpl().wineInfoInsert(conn, new WineInfo(wineInfoName, wineInfoProfilePicture, wineInfoPrice, wineInfoOrigin, wineInfoPicture1, wineInfoPicture2, wineInfoPicture3, wineSellerId));
+			assertThat(result, is(1));
+		} 
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//	나머지 테스트는.. DB정보 부족으로 인한...
 }
