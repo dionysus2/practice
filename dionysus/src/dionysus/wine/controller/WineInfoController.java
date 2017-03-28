417e2a85e492f19620ee41b7ef85ae575cc784da
Administrator <Administrator@Admin-PC>
2017-03-29 08:51:17 +0900

    추가

--- dionysus/src/dionysus/wine/controller/WineInfoController.java
+++ dionysus/src/dionysus/wine/controller/WineInfoController.java
@@ -10,17 +10,6 @@ import dionysus.wine.di.RequestMapping;
 import dionysus.wine.serviceimpl.WineInfoServiceImpl;
 
 public class WineInfoController {
-	/*											
-	 *	와인상품 전체리스트 조회		
-	 *	단가높은순 조회					
-	 *	단가낮은순 조회					
-	 *	와인상품 원산지별 조회			
-	 *	와인상품 추가						
-	 *	와인상품 정보수정					
-	 *	와인상품 삭제							
-	 *	회사리스트별 상품조회 	
-	 *	와인번호별 와인상품 상세정보 조회
-	 */
 	// 상품 전체리스트 조회하기
 	private static Logger logger= LoggerFactory.getLogger(WineInfoController.class);
 	@RequestMapping(value="/wineinfo/list", method="GET")
@@ -43,7 +32,7 @@ public class WineInfoController {
 		return mav;
 	}
 
-	//	상품 전체리스트 원산지별 조회하기 
+	//	상품 전체리스트 원산지별 조회하기  => 경로 추가 필요합니다
 	@RequestMapping(value="/wineinfo/originlist", method="GET")
 	public static ModelAndView readByWineInfoOrigin(HttpServletRequest request){
 		ModelAndView mav= new ModelAndView();
@@ -52,7 +41,7 @@ public class WineInfoController {
 		mav.addObject("result", service.readCountryWineInfo(request));
 		return mav;
 	}
-	//	단가 높은순으로 상품 전체조회하기
+	//	단가 높은순으로 상품 전체조회하기 => 경로 추가 필요합니다
 	@RequestMapping(value="/wineinfo/listofpricemax", method="GET")
 	public static ModelAndView readByWineInfoPriceMax(HttpServletRequest request){
 		ModelAndView mav= new ModelAndView();
@@ -61,7 +50,7 @@ public class WineInfoController {
 		mav.addObject("result", service.readPriceMax(request));
 		return mav;
 	}
-	//	단가 낮은순으로 상품 전체 조회하기
+	//	단가 낮은순으로 상품 전체 조회하기  => 경로 추가 필요합니다
 	@RequestMapping(value="/wineinfo/listofpricemin", method="GET")
 	public static ModelAndView readByWineInfoPriceMin(HttpServletRequest request){
 		ModelAndView mav= new ModelAndView();
@@ -70,12 +59,23 @@ public class WineInfoController {
 		mav.addObject("result", service.readPriceMin(request));
 		return mav;
 	}
+	//	와인상품 생산국가별 조회  => 경로 추가 필요합니다
+	@RequestMapping(value="/wineinfo/listofcountry", method="GET")
+	public static ModelAndView readByCountry(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.setView("#.jsp");
+		mav.addObject("result", service.readCountryWineInfo(request));
+		return mav;
+	}
+	//	와인상품 추가 폼 뷰	  => 경로 수정 필요합니다
 	@RequestMapping(value="/wineinfo/insert", method="GET")
 	public static ModelAndView insertStart(HttpServletRequest request){
 		ModelAndView mav= new ModelAndView();
 		mav.setView("/jaehyuntest/insert.jsp");
 		return mav;
 	}
+	//	와인상품 추가완료 => 경로 수정 필요합니다
 	@RequestMapping(value="/wineinfo/insert", method="POST")
 	public static ModelAndView insertEnd(HttpServletRequest request){
 		ModelAndView mav= new ModelAndView();
@@ -90,6 +90,58 @@ public class WineInfoController {
 		else{
 			logger.info("추가안됨.");
 			mav.setView("insert");
+			mav.setRedirect();
+			return mav;
+		}
+	}
+	//	회사리스트별 상품조회 => 경로 추가 필요합니다
+	@RequestMapping(value="/wineinfo/listofwineseller", method="GET")
+	public static ModelAndView readByWineSellerWineInfo(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.setView("#.jsp");
+		mav.addObject("result", service.readWineSellerWineInfo(request));
+		return mav;
+	}
+	//	정보수정 폼 뷰 => 경로 추가 필요합니다
+	@RequestMapping(value="/wineinfo/update", method="GET")
+	public static ModelAndView updateStart(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.setView("#.jsp");
+		mav.addObject("result", service.wineInfoUpdateStart(request));
+		return mav;
+	}
+	//	정보수정 완료 => 경로 수정 필요합니다
+	@RequestMapping(value="/wineinfo/update", method="POST")
+	public static ModelAndView updateEnd(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.addObject("result", service.wineInfoUpdateEnd(request));
+		if(mav.getObject("result").equals(true)){
+			mav.setView("list");
+			mav.setRedirect();
+			return mav;
+		}
+		else{
+			mav.setView("update");
+			mav.setRedirect();
+			return mav;
+		}
+	}
+	//	와인정보 삭제 => 경로수정 및 로직검토 필요합니다.
+	@RequestMapping(value="/wineinfo/delete", method="GET")
+	public static ModelAndView delete(HttpServletRequest request){
+		ModelAndView mav= new ModelAndView();
+		WineInfoServiceImpl service= (WineInfoServiceImpl)request.getServletContext().getAttribute("wineinfoservice");
+		mav.addObject("result", service.wineInfoDelete(request));
+		if(mav.getObject("result").equals(true)){
+			mav.setView("list");
+			mav.setRedirect();
+			return mav;
+		}
+		else{
+			mav.setView("list");
 			mav.setRedirect();
 			return mav;
 		}
--- dionysus/src/dionysus/wine/service/WineOrderService.java
+++ dionysus/src/dionysus/wine/service/WineOrderService.java
@@ -1,5 +1,39 @@
 package dionysus.wine.service;
 
-public interface WineOrderService {
+import javax.servlet.http.HttpServletRequest;
 
+public interface WineOrderService {
+	/*
+	 * 	- 와인상품 주문전체 리스트 조회								리턴					매개변수
+	 * 		1. 와인상품 주문전체 리스트 페이지별 조회			ArrayList<WineOrder>		Connection, 게시글, 게시글
+	 * 		2. 와인상품 주문전체 리스트 count조회					int						Connection
+	 * 	- 월별/일별 주문 리스트 조회
+	 * 		3. 주문일자별(월별)주문 리스트 조회					ArrayList<WineOrder>		Connection, 주문일자(월별)
+	 * 		4. 주문일자별(일별)주문 리스트 조회					ArrayList<WineOrder>		Connection, 주문일자(일별)
+	 * 	- 와인상품 레스토랑별 주문 리스트 조회
+	 * 		5. 와인상품 레스토랑별 주문 리스트 조회		ArrayList<WineOrder>	Connection, 레스토랑번호
+	 * 	- 와인회사별 상품 주문건 판매량 조회
+	 * 		6. 와인회사별 상품 주문건 전체 판매량 조회			ArrayList<WineOrder>		Connection
+	 * 	- 월별/일별 상품 주문건 판매량 조회
+	 * 		7. 와인회사별/월별 상품 주문건 판매량 조회	ArrayList<Integer>	Connection, 주문일자(월별), 와인회사번호
+	 * 		8. 와인회사별/일별 상품 주문건 판매량 조회	ArrayList<Integer>	Connection, 주문일자(일별), 와인회사번호
+	 * 	- 와인상품 주문회원 정보조회
+	 * 		9. 와인상품 주문회원 정보조회						ArryList<Customer>			Connection, 와인회사번호, 고객번호
+	 * 	- 주문건 등록.
+	 * 		10. 주문건 등록											int						Connection, WineOrder
+	 * 	- 주문건 삭제.
+	 * 		11. 주문건 삭제											int						Connection, 주문번호
+	 * 	- 주문건수정(주문상세에서 처리)
+	 * */
+	public String readAllWineOrderList(HttpServletRequest request);
+	public String readByWineOrderDateMonth(HttpServletRequest request);
+	public String readByWineOrderDateDay(HttpServletRequest request);
+	public String readByWineSellerWineOrder(HttpServletRequest request);
+	public String readByWineSellerWineOrderAmountSum(HttpServletRequest request);
+	public String readByWineSellerWineOrderMonth(HttpServletRequest request);
+	public String readByWineSellerWineOrderDay(HttpServletRequest request);
+	public String readByCustomerWineOrder(HttpServletRequest request);
+	public String createStart(HttpServletRequest request);
+	public String createEnd(HttpServletRequest request);
+	public String delete(HttpServletRequest request);
 }
