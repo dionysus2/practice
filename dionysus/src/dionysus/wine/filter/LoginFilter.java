b88f6c9bfe95d05437f3eebf8bfaab6fe3c8ef46
Administrator <Administrator@Admin-PC>
2017-04-04 11:55:07 +0900

    134

--- dionysus/src/dionysus/wine/filter/LoginFilter.java
+++ dionysus/src/dionysus/wine/filter/LoginFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebServlet({"/main/*", "/basic/*", "/wineinfo/*", "/res/*", "/resinfo/*", "/customer/*", "/manager/*", "/notice/*", "/wineinfo/wineorder/*"})
-
 public class LoginFilter implements Filter {
 	private Logger logger= LoggerFactory.getLogger(LoginFilter.class);
 	private ArrayList<String> whiteList= new ArrayList<String>();
